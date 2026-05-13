aa010e2fc347bd0158c525652b2df130f892d272
JohannesL2 <johannes.lindgren2002@gmail.com>
2026-05-13 21:03:19 +0200

    Change PokemonList so it shows 2 pokemons each column on mobile devices

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -18,7 +18,7 @@ const handleLoadMore = async () => {
 </script>
 
 <template>
-    <div v-if="pokemons.length > 0" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <div v-if="pokemons.length > 0" class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 md:gap-8">
         <PokemonCard
             v-for="p in pokemons"
             :key="p.id"
