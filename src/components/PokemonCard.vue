a6d8f375d0a29d641157bc320216b0637abe1796
JohannesL2 <johannes.lindgren2002@gmail.com>
2026-05-13 22:17:11 +0200

    Make pokemon images responsive and prevent squishing on mobile

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -9,13 +9,13 @@ defineProps<{
 <template>
     <router-link :to="{ name: 'pokemon-detail', params: { name: pokemon.name } }" class="block">
     <div class="group border p-4 rounded-xl shadow-sm hover:shadow-xl hover:-translate-y-1 transition-all duration-300 bg-white">
-      <div class="relative flex justify-center bg-slate-50 rounded-lg p-4 mb-4 overflow-hidden">
+      <div class="relative flex justify-center bg-slate-50 rounded-lg p-2 sm:p-4 mb-4 overflow-hidden">
         <div class="absolute inset-0 bg-slate-200/50 rounded-full scale-75 group-hover:scale-90 transition-transform duration-500"></div>
         
         <img 
           :src="pokemon.sprites.front_default" 
           :alt="pokemon.name" 
-          class="relative w-32 h-32 z-10 pixelated"
+          class="relative w-full max-w-[128px] aspect-square object-contain z-10 pixelated"
         />
       </div>
       
